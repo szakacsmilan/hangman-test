5a90374955a0fd532e66dbe8edd1b76890b18bbc
Sütő Bence 'Owen <ssben636@gmail.com>
2018-11-21 10:55:10 +0100

    safety commit, pep8

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'malac'
 
+
 @app.route('/game', methods=['GET', 'POST'])
 def game_page():
     return render_template('game.html')
